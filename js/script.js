b4503a58ca03f8b24742c28ffffff9591cfb5dac
mohammad-shoaibb <136231433+mohammad-shoaibb@users.noreply.github.com>
2023-09-30 16:12:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//js
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+    }
+    )
+    wow.init();
+
+//jq
+$(document).ready(function(){
+
+
+    //counterUp start
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+
+
+
+
+})
